1b7f977987dcf50b8febe8a2ff77a4816aaa5699
oluomojossy <oluomojossy@gmail.com>
2024-03-13 14:49:11 +0100

    second commit

--- src/About.jsx
+++ src/About.jsx
@@ -1,4 +1,5 @@
 import './About.css'
+import svgOne from './assets/1.svg'
 
 function About (){
     return(
@@ -9,7 +10,8 @@ function About (){
                 <p className='selfp'>Hello I’m a software developer! I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.</p>
                 <p className='selfp2'>LET'S CONNECT</p>
                 <div className="Icons2">
-                <img src='./src/assets/1.svg'/>
+                {/* <img src='./src/assets/1.svg'/> */}
+                <img src={svgOne} alt="" />
                 <img src='./src/assets/2.svg'/>
                 <img src='./src/assets/3.svg'/>
                 <img src='./src/assets/4.svg'/>
